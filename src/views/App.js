3efe381b6dce501ebbeacb86bc8216873b08d029
Bo Cuoi <ndkhanh.18it4@vku.udn.vn>
2023-11-02 22:14:32 +0700

    Form

--- src/views/App.js
+++ src/views/App.js
@@ -8,17 +8,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello world!!!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
         <MyComponent />
       </header>
     </div>
--- src/views/Example/MyComponent.js
+++ src/views/Example/MyComponent.js
@@ -1,9 +1,53 @@
 import React from "react";
 
 class MyComponent extends React.Component {
+    state = {
+        masv: '',
+        password: ''
+    }
+
+    handleChangeMasv = (event) => {
+        this.setState({
+            masv: event.target.value
+        })
+    }
+
+    handleChangePassword = (event) => {
+        this.setState({
+            password: event.target.value
+        })
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault()
+        alert(this.state.masv)
+    }
+
     render() {
         return (
-            <div>Hello everyone</div>
+            <>
+                <form>
+                    <label>Mã sinh viên:</label><br />
+                    <input
+                        type="text"
+                        value={this.state.masv}
+                        onChange={(event) => this.handleChangeMasv(event)}
+                    />
+                    <br />
+                    <label>Mật khẩu:</label><br />
+                    <input
+                        type="password"
+                        value={this.state.password}
+                        onChange={(event) => this.handleChangePassword(event)}
+                    />
+                    <br />
+                    <input
+                        type="submit"
+                        value="Đăng nhập"
+                        onClick={(event) => this.handleSubmit(event)}
+                    />
+                </form>
+            </>
         )
     }
 }
